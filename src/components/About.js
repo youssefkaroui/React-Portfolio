10d0cf67be8b8f7bd9af7c7399a9529ad607c58f
Youssef <youssefkaroui6@gmail.com>
2023-10-08 18:08:31 -0400

    About section done

---
+++ src/components/About.js
@@ -0,0 +1,26 @@
+import React from 'react';
+
+function About() {
+
+    return(
+        <section className="container">
+            <h2 class="top-title">Youssef Karoui</h2>
+            <hr></hr>
+            <div>
+                <img class="mb-5" id="avatar" src="../assets/images/avatar.png" alt="Youssef Karoui" />
+
+                <p>
+                College graduate with a bachlors in Bio-engineering. Currenty working in customer service feild, spesifically in the restaurant industry. 
+                </p>
+                <p>
+                Strong customer service expereince. With over 8 years of experience in the feild with a variety of companies.
+                </p>
+                <p>
+                Proven track of leadership roles in the customer service feild.
+                </p>
+            </div>
+        </section>
+    )
+}
+
+export default About;
