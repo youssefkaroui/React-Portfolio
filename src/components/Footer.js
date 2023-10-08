baf6cda3b3a20c2ed034e79903bc98252caadcab
Youssef <youssefkaroui6@gmail.com>
2023-10-08 18:18:15 -0400

    footer.js done

---
+++ src/components/Footer.js
@@ -0,0 +1,14 @@
+
+import React from 'react';
+
+function Footer() {   
+    return (
+    <footer className="footer bg-dark fixed-bottom">
+        <a href="https://github.com/youssefkaroui"><img src="https://img.icons8.com/color/48/000000/github--v1.png" alt="Github" className="icon"/></a>
+        <a href="https://www.linkedin.com/in/youssef-karoui-930877121/"><img src="https://img.icons8.com/fluent/48/000000/linkedin.png" alt="LinkedIn" className="icon"/></a>
+        <a href="https://stackoverflow.com/users/18113354/youssef-karoui"><img src="https://upload.wikimedia.org/wikipedia/commons/e/ef/Stack_Overflow_icon.svg" alt="MichaelGiddings.com" className="icon"/></a>
+    </footer>
+    )
+}
+
+export default Footer;
