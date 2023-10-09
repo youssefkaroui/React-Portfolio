e9de30e6e7df1e2c5dd090210e09404b7cc6a8c3
Youssef <youssefkaroui6@gmail.com>
2023-10-09 13:10:01 -0400

    added emailValidation helper function

---
+++ src/utils/helpers.js
@@ -0,0 +1,4 @@
+export function emailValidation(employerEmail) {
+    var options = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return options.test(String(employerEmail).toLowerCase());
+  }
